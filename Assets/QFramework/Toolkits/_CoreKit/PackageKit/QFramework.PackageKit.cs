b0c6334dbe3906fee79f0f49e171efb63f2f1e4a
nathanliao <liaont@guanglang.cn>
2025-01-10 11:16:23 +0800

    v1.0.170

---
+++ Assets/QFramework/Toolkits/_CoreKit/PackageKit/QFramework.PackageKit.cs
@@ -0,0 +1,498 @@
+/****************************************************************************
+ * Copyright (c) 2016 ~ 2022 liangxiegame UNDER MIT LICENSE
+ * 
+ * https://qframework.cn
+ * https://github.com/liangxiegame/QFramework
+ * https://gitee.com/liangxiegame/QFramework
+ ****************************************************************************/
+
+#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+
+namespace QFramework
+{
+    public static class FrameworkMenuItems
+    {
+        public const string Preferences = "QFramework/Preferences... %e";
+    }
+
+    public static class FrameworkMenuItemsPriorities
+    {
+        public const int Preferences = 1;
+    }
+    
+
+    public class SubWindow : EditorWindow, IMGUILayout
+    {
+        public string Id { get; set; }
+        public bool Visible { get; set; }
+
+        public Func<bool> VisibleCondition { get; set; }
+
+        void IMGUIView.DrawGUI()
+        {
+        }
+
+        IMGUILayout IMGUIView.Parent { get; set; }
+
+        private FluentGUIStyle mStyle = new FluentGUIStyle(() => new GUIStyle());
+
+        public FluentGUIStyle Style
+        {
+            get { return mStyle; }
+            set { mStyle = value; }
+        }
+
+        Color IMGUIView.BackgroundColor { get; set; }
+
+
+        private List<IMGUIView> mPrivateChildren = new List<IMGUIView>();
+
+        private List<IMGUIView> mChildren
+        {
+            get { return mPrivateChildren; }
+            set { mPrivateChildren = value; }
+        }
+
+        void IMGUIView.RefreshNextFrame()
+        {
+        }
+
+        void IMGUIView.AddLayoutOption(GUILayoutOption option)
+        {
+        }
+
+        void IMGUIView.RemoveFromParent()
+        {
+        }
+
+        void IMGUIView.Refresh()
+        {
+        }
+
+        public void Hide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMGUILayout AddChild(IMGUIView view)
+        {
+            mChildren.Add(view);
+            view.Parent = this;
+            return this;
+        }
+
+        public void RemoveChild(IMGUIView view)
+        {
+            mChildren.Add(view);
+            view.Parent = null;
+        }
+
+        public void Clear()
+        {
+            mChildren.Clear();
+        }
+
+        private void OnGUI()
+        {
+            mChildren.ForEach(view => view.DrawGUI());
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public abstract class Window : EditorWindow, IDisposable
+    {
+        public static Window MainWindow { get; protected set; }
+
+        public IMGUIViewController ViewController { get; set; }
+
+        public T CreateViewController<T>() where T : IMGUIViewController, new()
+        {
+            var t = new T();
+            t.SetUpView();
+            return t;
+        }
+
+        public static void Open<T>(string title) where T : Window
+        {
+            MainWindow = GetWindow<T>(true);
+
+            if (!MainWindow.mShowing)
+            {
+                MainWindow.position = new Rect(Screen.width / 2, Screen.height / 2, 800, 600);
+                MainWindow.titleContent = new GUIContent(title);
+                MainWindow.Init();
+                MainWindow.mShowing = true;
+                MainWindow.Show();
+            }
+            else
+            {
+                MainWindow.mShowing = false;
+                MainWindow.Dispose();
+                MainWindow.Close();
+                MainWindow = null;
+            }
+        }
+
+        public static SubWindow CreateSubWindow(string name = "SubWindow")
+        {
+            var window = GetWindow<SubWindow>(true, name);
+            window.Clear();
+            return window;
+        }
+
+        void Init()
+        {
+            OnInit();
+        }
+
+
+        public void PushCommand(System.Action command)
+        {
+            RenderEndCommandExecutor.PushCommand(command);
+        }
+
+        private void OnGUI()
+        {
+            if (ViewController != null)
+            {
+                ViewController.View.DrawGUI();
+            }
+
+            RenderEndCommandExecutor.ExecuteCommand();
+        }
+
+        public void Dispose()
+        {
+            OnDispose();
+        }
+
+        protected bool mShowing = false;
+
+
+        protected abstract void OnInit();
+        protected abstract void OnDispose();
+    }
+
+
+    public static class WindowExtension
+    {
+        public static T PushCommand<T>(this T view, System.Action command) where T : IMGUIView
+        {
+            RenderEndCommandExecutor.PushCommand(command);
+            return view;
+        }
+    }
+
+    public static class SubWindowExtension
+    {
+        public static T Postion<T>(this T subWindow, int x, int y) where T : SubWindow
+        {
+            var rect = subWindow.position;
+            rect.x = x;
+            rect.y = y;
+            subWindow.position = rect;
+
+            return subWindow;
+        }
+
+        public static T Size<T>(this T subWindow, int width, int height) where T : SubWindow
+        {
+            var rect = subWindow.position;
+            rect.width = width;
+            rect.height = height;
+            subWindow.position = rect;
+
+            return subWindow;
+        }
+
+        public static T PostionScreenCenter<T>(this T subWindow) where T : SubWindow
+        {
+            var rect = subWindow.position;
+            rect.x = Screen.width / 2;
+            rect.y = Screen.height / 2;
+            subWindow.position = rect;
+
+            return subWindow;
+        }
+    }
+
+    public static class EditorUtils
+    {
+        public static string GetSelectedPathOrFallback()
+        {
+            var path = string.Empty;
+
+            foreach (var obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
+            {
+                path = AssetDatabase.GetAssetPath(obj);
+
+                if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                {
+                    return path;
+                }
+            }
+
+            return path;
+        }
+
+        public static void MarkCurrentSceneDirty()
+        {
+            EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+        }
+
+        public static string CurrentSelectPath
+        {
+            get { return Selection.activeObject == null ? null : AssetDatabase.GetAssetPath(Selection.activeObject); }
+        }
+
+        public static string AssetsPath2ABSPath(string assetsPath)
+        {
+            string assetRootPath = Path.GetFullPath(Application.dataPath);
+            return assetRootPath.Substring(0, assetRootPath.Length - 6) + assetsPath;
+        }
+
+        public static string ABSPath2AssetsPath(string absPath)
+        {
+            string assetRootPath = Path.GetFullPath(Application.dataPath);
+            Debug.Log(assetRootPath);
+            Debug.Log(Path.GetFullPath(absPath));
+            return "Assets" + Path.GetFullPath(absPath).Substring(assetRootPath.Length).Replace("\\", "/");
+        }
+
+
+        public static string AssetPath2ReltivePath(string path)
+        {
+            if (path == null)
+            {
+                return null;
+            }
+
+            return path.Replace("Assets/", "");
+        }
+
+        public static bool ExcuteCmd(string toolName, string args, bool isThrowExcpetion = true)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = toolName;
+            process.StartInfo.Arguments = args;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            OuputProcessLog(process, isThrowExcpetion);
+            return true;
+        }
+
+        public static void OuputProcessLog(Process p, bool isThrowExcpetion)
+        {
+            string standardError = string.Empty;
+            p.BeginErrorReadLine();
+
+            p.ErrorDataReceived += (sender, outLine) => { standardError += outLine.Data; };
+
+            string standardOutput = string.Empty;
+            p.BeginOutputReadLine();
+            p.OutputDataReceived += (sender, outLine) => { standardOutput += outLine.Data; };
+
+            p.WaitForExit();
+            p.Close();
+
+            Debug.Log(standardOutput);
+            if (standardError.Length > 0)
+            {
+                if (isThrowExcpetion)
+                {
+                    Debug.Log(standardError);
+                    throw new Exception(standardError);
+                }
+
+                Debug.Log(standardError);
+            }
+        }
+
+        public static Dictionary<string, string> ParseArgs(string argString)
+        {
+            int curPos = argString.IndexOf('-');
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            while (curPos != -1 && curPos < argString.Length)
+            {
+                int nextPos = argString.IndexOf('-', curPos + 1);
+                string item = string.Empty;
+
+                if (nextPos != -1)
+                {
+                    item = argString.Substring(curPos + 1, nextPos - curPos - 1);
+                }
+                else
+                {
+                    item = argString.Substring(curPos + 1, argString.Length - curPos - 1);
+                }
+
+                item = StringTrim(item);
+                int splitPos = item.IndexOf(' ');
+
+                if (splitPos == -1)
+                {
+                    string key = StringTrim(item);
+                    result[key] = "";
+                }
+                else
+                {
+                    string key = StringTrim(item.Substring(0, splitPos));
+                    string value = StringTrim(item.Substring(splitPos + 1, item.Length - splitPos - 1));
+                    result[key] = value;
+                }
+
+                curPos = nextPos;
+            }
+
+            return result;
+        }
+
+        public static string GetFileMD5Value(string absPath)
+        {
+            if (!File.Exists(absPath))
+                return "";
+
+            MD5CryptoServiceProvider md5CSP = new MD5CryptoServiceProvider();
+            FileStream file = new FileStream(absPath, FileMode.Open);
+            byte[] retVal = md5CSP.ComputeHash(file);
+            file.Close();
+            string result = "";
+
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                result += retVal[i].ToString("x2");
+            }
+
+            return result;
+        }
+
+
+        public static string StringTrim(string str, params char[] trimer)
+        {
+            int startIndex = 0;
+            int endIndex = str.Length;
+
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if (!IsInCharArray(trimer, str[i]))
+                {
+                    startIndex = i;
+                    break;
+                }
+            }
+
+            for (int i = str.Length - 1; i >= 0; --i)
+            {
+                if (!IsInCharArray(trimer, str[i]))
+                {
+                    endIndex = i;
+                    break;
+                }
+            }
+
+            if (startIndex == 0 && endIndex == str.Length)
+            {
+                return string.Empty;
+            }
+
+            return str.Substring(startIndex, endIndex - startIndex + 1);
+        }
+
+        public static string StringTrim(string str)
+        {
+            return StringTrim(str, ' ', '\t');
+        }
+
+        static bool IsInCharArray(char[] array, char c)
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == c)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static class MouseSelector
+    {
+        public static string GetSelectedPathOrFallback()
+        {
+            var path = string.Empty;
+
+            foreach (var obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
+            {
+                path = AssetDatabase.GetAssetPath(obj);
+                if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                {
+                }
+            }
+
+            return path;
+        }
+    }
+
+
+    internal class ColorView : IMGUIAbstractView
+    {
+        public ColorView(Color color)
+        {
+            Color = new BindableProperty<Color>(color);
+        }
+
+        public BindableProperty<Color> Color { get; private set; }
+
+        protected override void OnGUI()
+        {
+            Color.Value = EditorGUILayout.ColorField(Color.Value, LayoutStyles);
+        }
+    }
+
+
+    internal class EnumPopupView : IMGUIAbstractView
+    {
+        public BindableProperty<Enum> ValueProperty { get; set; }
+
+        public EnumPopupView(Enum initValue)
+        {
+            ValueProperty = new BindableProperty<Enum>(initValue);
+            ValueProperty.Value = initValue;
+            Style = new FluentGUIStyle(() => EditorStyles.popup);
+        }
+
+        protected override void OnGUI()
+        {
+            Enum enumType = ValueProperty.Value;
+            ValueProperty.Value = EditorGUILayout.EnumPopup(enumType, Style.Value, LayoutStyles);
+        }
+    }
+
+
+    public abstract class IMGUIViewController
+    {
+        public VerticalLayout View = new VerticalLayout();
+
+        public abstract void SetUpView();
+    }
+}
+#endif
